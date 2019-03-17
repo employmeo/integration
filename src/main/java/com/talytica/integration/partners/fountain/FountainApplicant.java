b3a7c7fa5ca02a7a12614c38ede23588bc1d14d9
sri @ laptop <sridharkaza@gmail.com>
2019-03-17 10:50:24 -0700

    added fountain integration

---
+++ src/main/java/com/talytica/integration/partners/fountain/FountainApplicant.java
@@ -0,0 +1,26 @@
+package com.talytica.integration.partners.fountain;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FountainApplicant {
+	
+	private String email;
+    private String name;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
+    private Date created_at;
+	private Boolean receive_automated_emails;
+	private List<String> labels;
+	private String id;
+
+}
---
+++ src/main/java/com/talytica/integration/resources/FountainResource.java
@@ -0,0 +1,131 @@
+package com.talytica.integration.resources;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Range;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.employmeo.data.model.*;
+import com.employmeo.data.service.AccountService;
+import com.employmeo.data.service.CorefactorService;
+import com.employmeo.data.service.GraderService;
+import com.employmeo.data.service.PartnerService;
+import com.employmeo.data.service.RespondantService;
+import com.google.common.collect.Lists;
+import com.talytica.common.service.ExternalLinksService;
+import com.talytica.integration.objects.*;
+import com.talytica.integration.partners.FountainPartnerUtil;
+import com.talytica.integration.partners.GreenhousePartnerUtil;
+import com.talytica.integration.partners.PartnerUtil;
+import com.talytica.integration.partners.PartnerUtilityRegistry;
+import com.talytica.integration.partners.fountain.FountainWebHook;
+import com.talytica.integration.partners.greenhouse.GreenhouseApplication;
+import com.talytica.integration.partners.greenhouse.GreenhouseAssessmentOrder;
+import com.talytica.integration.partners.greenhouse.GreenhouseErrorNotice;
+import com.talytica.integration.partners.greenhouse.GreenhousePolling;
+import com.talytica.integration.partners.greenhouse.GreenhouseStatusResponse;
+import com.talytica.integration.partners.greenhouse.GreenhouseWebHook;
+
+import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/v1/fountain")
+@Api( value="/v1/fountain", produces=MediaType.APPLICATION_JSON, consumes=MediaType.APPLICATION_JSON)
+
+public class FountainResource {
+	
+
+
+	private static final ResponseBuilder ACCOUNT_NOT_FOUND = Response.status(Response.Status.NOT_FOUND).entity("Account Not Found");
+	private static final ResponseBuilder RESPONDANT_NOT_FOUND = Response.status(Response.Status.NOT_FOUND).entity("Candidate Not Found");
+	private static final ResponseBuilder ACCOUNT_MATCH = Response.status(Response.Status.CONFLICT).entity("{ message: 'Applicant ID not found for Account ID' }");
+	
+	@Context
+	private SecurityContext sc;
+	@Autowired
+	private PartnerService partnerService;
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private RespondantService respondantService;
+	@Autowired
+	private GraderService graderService;
+	@Autowired
+	private CorefactorService corefactorService;
+	@Autowired
+	private ExternalLinksService externalLinksService;
+	@Autowired
+	private PartnerUtilityRegistry partnerUtilityRegistry;
+
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Order new assessment and create candidate")
+	   @ApiResponses(value = {
+		  @ApiResponse(code = 200, message = "Request Processed"),
+		  @ApiResponse(code = 401, message = "API key not accepted"),
+		  @ApiResponse(code = 404, message = "Account Not Found")
+	   })
+	@Path("/webhook")
+	public Response postOrder(@ApiParam (value = "WebHook", type="FountainWebHook")  @RequestBody String webhook) throws JSONException {
+		log.info(webhook);
+		
+		return Response.status(Response.Status.OK).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Order new assessment and create candidate")
+	   @ApiResponses(value = {
+		  @ApiResponse(code = 200, message = "Request Processed"),
+		  @ApiResponse(code = 401, message = "API key not accepted"),
+		  @ApiResponse(code = 404, message = "Account Not Found")
+	   })
+	@Path("/webhooknew")
+	public Response postOrder(@ApiParam (value = "WebHook", type="FountainWebHook")  @RequestBody FountainWebHook webhook) throws JSONException {
+		Partner partner = partnerService.getPartnerByLogin(sc.getUserPrincipal().getName());
+		Account account = accountService.getByPartnerId(partner.getId());
+		FountainPartnerUtil fpu = (FountainPartnerUtil) partnerUtilityRegistry.getUtilFor(partner);
+		if (account == null)throw new WebApplicationException(ACCOUNT_NOT_FOUND.build());
+		
+		JSONObject jOrder = webhook.toJson();
+		if (null != partner.getApiKey()) {
+			log.debug("Setting up for postback");
+			JSONObject delivery = new JSONObject();
+
+			String appId = webhook.getApplicant().getId();
+			if (null != appId) {
+				String scorePostMethod = fpu.getApplicantUpdateMethod(appId);
+				delivery.put("scores_post_url", scorePostMethod);
+				jOrder.put("delivery", delivery);
+			}
+		}
+		fpu.createRespondantFrom(jOrder, account);		
+		
+		return Response.status(Response.Status.OK).build();
+	}
+}
