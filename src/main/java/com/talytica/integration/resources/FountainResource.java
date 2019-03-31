b9692c12ca82eb7b64748a545b23be37619fbff3
sri @ laptop <sridharkaza@gmail.com>
2019-03-31 10:08:35 -0700

    added stagechange webhook with API key to replace the auth'd webhook

--- src/main/java/com/talytica/integration/resources/FountainResource.java
+++ src/main/java/com/talytica/integration/resources/FountainResource.java
@@ -1,48 +1,22 @@
 package com.talytica.integration.resources;
 
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Range;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.employmeo.data.model.*;
 import com.employmeo.data.service.AccountService;
-import com.employmeo.data.service.CorefactorService;
-import com.employmeo.data.service.GraderService;
 import com.employmeo.data.service.PartnerService;
-import com.employmeo.data.service.RespondantService;
-import com.google.common.collect.Lists;
-import com.talytica.common.service.ExternalLinksService;
-import com.talytica.integration.objects.*;
 import com.talytica.integration.partners.FountainPartnerUtil;
-import com.talytica.integration.partners.GreenhousePartnerUtil;
-import com.talytica.integration.partners.PartnerUtil;
 import com.talytica.integration.partners.PartnerUtilityRegistry;
 import com.talytica.integration.partners.fountain.FountainWebHook;
-import com.talytica.integration.partners.greenhouse.GreenhouseApplication;
-import com.talytica.integration.partners.greenhouse.GreenhouseAssessmentOrder;
-import com.talytica.integration.partners.greenhouse.GreenhouseErrorNotice;
-import com.talytica.integration.partners.greenhouse.GreenhousePolling;
-import com.talytica.integration.partners.greenhouse.GreenhouseStatusResponse;
-import com.talytica.integration.partners.greenhouse.GreenhouseWebHook;
 import com.talytica.integration.service.WorkflowService;
 
 import io.swagger.annotations.*;
@@ -56,12 +30,9 @@ import lombok.extern.slf4j.Slf4j;
 @Api( value="/v1/fountain", produces=MediaType.APPLICATION_JSON, consumes=MediaType.APPLICATION_JSON)
 
 public class FountainResource {
-	
-
 
 	private static final ResponseBuilder ACCOUNT_NOT_FOUND = Response.status(Response.Status.NOT_FOUND).entity("Account Not Found");
-	private static final ResponseBuilder RESPONDANT_NOT_FOUND = Response.status(Response.Status.NOT_FOUND).entity("Candidate Not Found");
-	private static final ResponseBuilder ACCOUNT_MATCH = Response.status(Response.Status.CONFLICT).entity("{ message: 'Applicant ID not found for Account ID' }");
+	private static final ResponseBuilder APIKEY_NOT_ACCEPTED = Response.status(Response.Status.UNAUTHORIZED).entity("API key not accepted");
 	
 	@Context
 	private SecurityContext sc;
@@ -84,14 +55,39 @@ public class FountainResource {
 		  @ApiResponse(code = 401, message = "API key not accepted"),
 		  @ApiResponse(code = 404, message = "Account Not Found")
 	   })
-	@Path("/webhookold")
-	public Response postOrder(@ApiParam (value = "WebHook", type="FountainWebHook")  @RequestBody String webhook) throws JSONException {
-		log.info(webhook);
+	@Path("/stagechange/{apiKey}")
+	public Response webhook(
+			@PathParam("apiKey") String apiKey,
+			@ApiParam (value = "WebHook", type="FountainWebHook")  @RequestBody FountainWebHook webhook
+			) throws JSONException {
+		Partner partner = partnerService.getPartnerByLogin(apiKey);
+		if (partner == null) return APIKEY_NOT_ACCEPTED.build();
+		FountainPartnerUtil fpu = (FountainPartnerUtil) partnerUtilityRegistry.getUtilFor(partner);
+
+		Account account = accountService.getByPartnerId(partner.getId());
+		if (account == null) return ACCOUNT_NOT_FOUND.build();
 		
+		JSONObject jOrder = webhook.toJson();
+		if (null != partner.getApiKey()) {
+			log.debug("Setting up for postback");
+			JSONObject delivery = new JSONObject();
+
+			String appId = webhook.getApplicant().getId();
+			if (null != appId) {
+				String scorePostMethod = fpu.getApplicantUpdateMethod(appId);
+				delivery.put("scores_post_url", scorePostMethod);
+				jOrder.put("delivery", delivery);
+			}
+		}
+		Respondant candidate = fpu.createRespondantFrom(jOrder, account);		
+		if (candidate.getRespondantStatus() == Respondant.STATUS_CREATED) {
+			workflowService.executeCreatedWorkflows(candidate);
+		}
 		return Response.status(Response.Status.OK).build();
 	}
 	
 	@POST
+	@Deprecated
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@ApiOperation(value = "Order new assessment and create candidate")
